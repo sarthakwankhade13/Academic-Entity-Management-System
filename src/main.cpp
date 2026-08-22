08e2e6d951b393e82659475023f076076f385267
sarthakwankhade13 <sarthakwankhade13@gmail.com>
2026-08-22 11:51:14 +0530

    Initialize project structure

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Academic Entity Management System" << std::endl;
+    std::cout << "Application started successfully!" << std::endl;
+
+    return 0;
+}
